4499c8b1938c783f76985da9f724de48e3a3dce2
Esmasen312 <esmasen312@gmail.com>
2020-07-30 07:06:28 -0500

    this is commit from other teammember

---
+++ src/test/java/qaclickacademy/New.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class New {
+
+	
+	@Test
+	public void Esma()
+	{
+		System.out.println("newproject");
+	}
+	
+	@Test
+	public void TeamMember()
+	{
+		System.out.println("new TeamMember");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Old.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Old {
+	
+	@Test
+	public void Lead()
+	{
+		System.out.println("our team lead");
+	}
+	
+	@BeforeTest
+	public void Project()
+	{
+		System.out.println("Project name is git");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
