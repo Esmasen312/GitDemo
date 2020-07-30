fa4f841c9a3182b74c11e067b0e42b927111d051
Esmasen312 <esmasen312@gmail.com>
2020-07-30 08:30:12 -0500

    this is new branch

---
+++ src/test/java/qaclickacademy/Branch.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Branch {
+
+	
+	@Test
+	public void develop()
+	{
+		System.out.println("new branch is develop");
+		System.out.println("we have 2 branches");
+		System.out.println("one is master, and second is develop");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
