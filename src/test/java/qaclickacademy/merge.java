e64265b8f4203e86be67512ee7723028070421ab
Esmasen312 <esmasen312@gmail.com>
2020-08-03 12:27:43 -0500

    new commit

--- src/test/java/qaclickacademy/merge.java
+++ src/test/java/qaclickacademy/merge.java
@@ -1,5 +1,12 @@
 package qaclickacademy;
 
+import org.testng.annotations.Test;
+
 public class merge {
 
+	@Test
+	public void merged()
+	{
+		System.out.println("merge develop + master branch");
+	}
 }
