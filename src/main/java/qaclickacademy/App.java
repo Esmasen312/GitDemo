566573e074a35b2fb47db1dcff320a2348ad9ea5
Esmasen312 <esmasen312@gmail.com>
2020-08-03 16:01:40 -0500

    now

--- src/main/java/qaclickacademy/App.java
+++ src/main/java/qaclickacademy/App.java
@@ -6,8 +6,13 @@ package qaclickacademy;
  */
 public class App 
 {
+	
+	String abc = "asdasd";
+	private final static String abcd = "asdsadasd";
+	
     public static void main( String[] args )
     {
+    	
         System.out.println( "Hello World!" );
     }
 }
